0b4e6c803e1b4e20277c12b6e46ee07c03161cff
Andrey <petukhovanvk@gmail.com>
2020-09-04 13:16:05 +0300

    first commit

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-spin-bj2it00sc6m"><div className="ldio-16twrlgaphl">
+        <div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div>
+        </div></div>
+    )
+}
+
+export default Spinner;
+
